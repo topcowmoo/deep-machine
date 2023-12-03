055f8d301d0ec8ae89de351adeecd0a8e7e2a3fd
Salvatore <mammoliti.sam@gmail.com>
2023-12-03 16:11:52 -0500

    "add high score function and sorting them"

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -2,8 +2,28 @@ let scoresBtn = document.querySelector(
     "#view-high-scores"
 );
 
+function printHighscores() {
+    let highscores = JSON.parse(
+        window.localStorage.getItem(
+            "highscores"
+        )
+    ) || [];
+    highscores.sort(
+        function(a, b) {
+            return b.score - a.score;
+        });
+        highscores.forEach (function (score) 
+        ) {
+            let liTag = document.createElement(
+                "li"
+                );
+            liTag.textContent = 
+            score.name + " - " + score.score;
 
 
+
+            
+        }
 
 
 
