5b49551f0d4c91c5557693696ff8f764517384cb
Salvatore <mammoliti.sam@gmail.com>
2023-12-04 11:03:36 -0500

    fixed syntax of highscore.html

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,41 +1,24 @@
-let scoresBtn = document.querySelector(
-    "#view-high-scores"
-);
+let scoresBtn = document.querySelector("#view-high-scores");
 
 function printHighscores() {
-    let highscores = JSON.parse(
-        window.localStorage.getItem(
-            "highscores"
-        )
-    ) || [];
-    highscores.sort(
-        function(a, b) {
-            return b.score - a.score;
-        });
-        highscores.forEach (function (score) 
-        ) {
-            let liTag = document.createElement(
-                "li"
-                );
-            liTag.textContent = 
-            score.name + " - " + score.score;
-        let olE1 =
-            document.getElementById(
-            "highscores"
-        );
-        olE1.appendChild(
-            liTag)
-        };
+    let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach (function(score) {
+        let liTag = document.createElement("li");
+            liTag.textContent = score.name + " - " + score.score;
+        let olE1 = document.getElementById("highscores");
+        olE1.appendChild(liTag);
+    });
 }
 
 function clearHighscores() {
-    window.localStorage.removeItem(
-        "highscores"
-    );
+    window.localStorage.removeItem("highscores");
     window.location.reload();
-    }
-    document.getElementById(
-        "clear"
-    ) .onclick = clearHighscores;
+}
+
+document.getElementById("clear") .onclick = clearHighscores;
 
 printHighscores();
