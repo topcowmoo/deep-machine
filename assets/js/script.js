d15137e55734e346fc77076d348fb3521c940cc8
Salvatore <mammoliti.sam@gmail.com>
2023-12-04 12:18:32 -0500

    moved event listener and array to top of script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,63 +7,49 @@ let questionsEl = document.querySelector("#questions");
     let feedbackEl = document.querySelector("#feedback");
     let reStartBtn = document.querySelector("#restart");
     
-document.addEventListener("DOMContentLoaded", function () {
-let questions = [
-    {
-        prompt: "In which HTML element do we put the JavaScript?",
-        options: [
-            "<js>",
-            "<javaScript>",
-            "<scripting>",
-            "<script>",
-        ],
-        correctAnswer: "<script>",
-    },
-
-    {
-        prompt: "Where is the correct place to insert a JavaScript?",
-        options: [
-            "The <head> section",
-            "The <footer> section",
-            "The <body> section",
-            "Both the <head> and the <body> sections",
-    ],
-        correctAnswer: "Both the <head> and the <body> sections",
-    },
-
-    {
-        prompt: "What is the correct way to write a JavaScript array?",
-        options: [
-            "var colors = (1:"red", 2:"green", 3:"blue")",
-            "var colors = ["red", "green", "blue"]",
-            "var colors = 1 = ("red, "green", "blue")",
-            "var colors = "red", "green", "blue",
-    ],
-        correctAnswer: "var colors = ["red", "green", "blue"]",
-    },
-
-    {
-        prompt: "How do you declare a JavaScript variable?",
-        options: [
-            "variable carName;",
-            "var carName;",
-            "call carName;",
-            "v carName;",
-    ],
-        correctAnswer: "var carName;",
-    },
-
-    {
-        prompt: "What will the following code return: Boolean(10>9)?",
-        options: [
-            "true",
-            "flase",
-            "Nan",
-            "Equal||",
-    ],
-        correctAnswer: "true",
-    },
-];
+    document.addEventListener("DOMContentLoaded", function () {
+        let questions = [
+            {
+                prompt: "In which HTML element do we put the JavaScript?",
+                options: ["<js>", "<javaScript>", "<scripting>", "<script>"],
+                correctAnswer: "<script>",
+            },
+            {
+                prompt: "Where is the correct place to insert JavaScript?",
+                options: [
+                    "The <head> section",
+                    "The <footer> section",
+                    "The <body> section",
+                    "Both the <head> and the <body> sections",
+                ],
+                correctAnswer: "Both the <head> and the <body> sections",
+            },
+            {
+                prompt: "What is the correct way to write a JavaScript array?",
+                options: [
+                    'var colors = ["red", "green", "blue"]',
+                    'var colors = (1:"red", 2:"green", 3:"blue")',
+                    'var colors = 1 = ("red", "green", "blue")',
+                    'var colors = "red", "green", "blue"',
+                ],
+                correctAnswer: 'var colors = ["red", "green", "blue"]',
+            },
+            {
+                prompt: "How do you declare a JavaScript variable?",
+                options: [
+                    "variable carName;",
+                    "var carName;",
+                    "call carName;",
+                    "v carName;",
+                ],
+                correctAnswer: "var carName;",
+            },
+            {
+                prompt: "What will the following code return: Boolean(10 > 9)?",
+                options: ["true", "false", "NaN", "Equal||"],
+                correctAnswer: "true",
+            },
+        ];
 
 function questionStart() {
     timerID = setInterval(
